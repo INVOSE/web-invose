6fbe4e942e5807dbe13734d7b92dfb518320019a
Nabilftd <nabilftd@gmail.com>
2015-02-15 22:59:54 +0700

    Memulai Mendeploy Web INVOSE

---
+++ application/controllers/nama_controller.php
@@ -0,0 +1,33 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+	 * INVOSE Project
+	 *
+	 * @author 		: INVOSE 2013-2015
+	 * @website 	: www.invose.com
+	 * @school 		: Islamic University of Indonesia
+	 * @company 	: Informatics Development of Website
+	 * @copyright	: 2015
+*/
+
+class nama_controller extends CI_CONTROLLER {
+
+	public function __construct() {
+		parent::__construct();
+		//Untuk Load Model dan lain-lain, taruh disini
+	}
+
+	/**
+	 * Deskripsi dari fungsi yang dibuat
+	 *
+	 * @param  something
+	 * @return something
+	 */
+    public function nama_fungsi() {
+    	return $something;
+    }
+
+}
+
+/* End of file nama_controller.php */
+/* Location: ./application/controllers/nama_controller.php */
---
+++ application/models/nama_model.php
@@ -0,0 +1,32 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+	 * INVOSE Project
+	 *
+	 * @author 		: INVOSE 2013-2015
+	 * @website 	: www.invose.com
+	 * @school 		: Islamic University of Indonesia
+	 * @company 	: Informatics Development of Website
+	 * @copyright	: 2015
+*/
+
+class nama_model extends CI_Model {
+
+    public function __construct() {
+        parent::__construct();
+        //Untuk Load Model dan lain-lain, taruh disini
+    }
+
+    /**
+	 * Deskripsi dari fungsi yang dibuat
+	 *
+	 * @param  something
+	 * @return something
+	 */
+    public function nama_fungsi() {
+    	return $something;
+    }
+   
+}
+
+?>
